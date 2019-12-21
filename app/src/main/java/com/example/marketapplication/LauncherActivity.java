c4dec65c67a2126067b611cfaec3728849b82ef9
Vikram Rayavarapu <46016457+rayavarapuvikram1@users.noreply.github.com>
2019-12-21 21:44:38 +0530

    Changed sign up to textview

--- app/src/main/java/com/example/marketapplication/LauncherActivity.java
+++ app/src/main/java/com/example/marketapplication/LauncherActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,7 +19,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LauncherActivity extends AppCompatActivity
 {
-    Button login_button, sign_up_button;
+    Button login_button;
+    TextView sign_up_button;
     FirebaseAuth firebaseAuth;
     EditText email_id_element, password_element;
     private FirebaseAuth.AuthStateListener authStateListener;
